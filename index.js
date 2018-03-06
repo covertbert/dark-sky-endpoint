941c2a71b349c51c86b59a77483e8961b1d8bfa0
Bertie Blackman <blackmanrgh@gmail.com>
2018-03-06 19:26:31 +0000

    now returns google image

--- index.js
+++ index.js
@@ -49,25 +49,35 @@ app.get('/location_data/json', (req, res) => {
   const lat = req.param('lat')
   const lon = req.param('lon')
   const url = 'https://maps.googleapis.com/maps/api/place/'
-  const placeDataQuery = `${url}nearbysearch/json?location=${lat},${lon}&radius=1000&key=${keys.images}`
+  const placeDataQuery = `${url}nearbysearch/json?location=${lat},${lon}&radius=1000&key=${
+    keys.images
+  }`
 
-  axios.get(placeDataQuery).then((response) => {
-    const placeData = response.data.results
-    // const randomNumberInArrayRange = Math.floor(Math.random() * (placeData.length - 0 + 1)) + 0
-    const randomLocationFromPlaceArray = placeData[1].place_id
-    const imageQuery = `${url}photo?maxwidth=400&photoreference=${randomLocationFromPlaceArray}&key=${keys.images}`
-    // const test = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=CnRtAAAATLZNl354RwP_9UKbQ_5Psy40texXePv4oAlgP4qNEkdIrkyse7rPXYGd9D_Uj1rVsQdWT4oRz4QrYAJNpFX7rzqqMlZw2h2E2y5IKMUZ7ouD_SlcHxYq1yL4KbKUv3qtWgTK0A6QbGh87GB3sscrHRIQiG2RrmU_jF4tENr9wGS_YxoUSSDrYjWmrNfeEHSGSc3FyhNLlBU&key=${keys.images}`
+  axios
+    .get(placeDataQuery)
+    .then(response => {
+      const placeData = response.data.results
 
-    axios.get(imageQuery).then((response) => {
-      console.log(response)
-      // res.send(CircularJSON.stringify(response))
-    }).catch(error => {
-      // res.send(error)
-      console.log('error')
+      const locationFromPlaceArray = placeData[0]
+      const imageFromLocationArray =
+        locationFromPlaceArray.photos[0].photo_reference
+
+      const imageQuery = `${url}photo?maxwidth=400&photoreference=${imageFromLocationArray}&key=${
+        keys.images
+      }`
+
+      axios
+        .get(imageQuery)
+        .then(response => {
+          res.send(CircularJSON.stringify(response))
+        })
+        .catch(error => {
+          res.send(error)
+        })
     })
-  }).catch(error => {
-    res.send(error)
-  })
+    .catch(error => {
+      res.send(error)
+    })
 })
 
 app.listen(3000, () => {
